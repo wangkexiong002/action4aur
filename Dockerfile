15cf764699e326cbeccf4985c1596f47adc47fa1
wangkexiong <wangkexiong@gmail.com>
2024-11-21 13:42:14 +0800

    suppress debug build generated

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,7 @@ RUN pacman -Syu base-devel git dos2unix --noconfirm --overwrite '*'
 RUN useradd -m build && usermod -aG wheel build && echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 RUN su - build -c 'git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -si --noconfirm; cd ..; rm -rf yay'
 
+COPY makepkg.conf  /home/build/.config/pacman/makepkg.conf
+
 ENTRYPOINT ["/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-mkdir -p /github/workspace/release
+set -e
+set -o pipefail
 
-if [ -f /github/workspace/customize.sh ]; then
-  dos2unix /github/workspace/customize.sh
-  chmod +x /github/workspace/customize.sh
-  /github/workspace/customize.sh
+WORKSPACE="/github/workspace"
+RELEASE_DIR="$WORKSPACE/release"
+CUSTOM_SCRIPT="$WORKSPACE/customize.sh"
+
+mkdir -p "${RELEASE_DIR}"
+
+if [ -f "${CUSTOM_SCRIPT}" ]; then
+  dos2unix "${CUSTOM_SCRIPT}"
+  chmod +x "${CUSTOM_SCRIPT}"
+  "${CUSTOM_SCRIPT}"
 else
   su - build -c "yay -Syu $1 --noconfirm"
 fi
 
-find /home/build/.cache -name *.zst | xargs -i mv {} /github/workspace/release
+if [ $? -eq 0 ]; then
+  find /home/build/.cache -name "*.zst" -exec mv {} "${RELEASE_DIR}" \;
+else
+  exit $?
+fi
 
